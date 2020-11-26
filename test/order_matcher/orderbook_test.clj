3503d91459637961f7f0bc865a5cf6b9d86a44ad
solaxun <markcwoodworth@gmail.com>
2020-11-26 00:09:52 -0600

    meh

--- test/order_matcher/orderbook_test.clj
+++ test/order_matcher/orderbook_test.clj
@@ -5,39 +5,22 @@
 
 (deftest limit-then-market
   "Expect a resting limit order to be cleared by market, resulting in empty book"
-  (let [b1 (new-fifo-book)
+  (let [b1 (atom (new-fifo-book))
         order1 (order {:order-type :limit
-                       :side       :buy
+                       :side       :bid
                        :price      103.1
                        :amount     5
                        :ticker     "aapl"})
         order2 (order {:order-type :market
-                       :side :sell
-                       :price 104.1
-                       :amount 5
-                       :ticker "aapl"})
-        _ (do (swap! book protocols/fill-order order1)
-              (swap! book protocols/fill-order order2))]
+                       :side       :ask
+                       :price      104.1
+                       :amount     5
+                       :ticker     "aapl"})
+        _ (do (swap! b1 protocols/fill-order order1)
+              (swap! b1 protocols/fill-order order2))]
 
     (testing
-        (is (and (= (:buy b1) {})                           ; book cleared
-                 (= (:sell b1) {}))))))                     ; all trades empty
-(limit-then-market)
+        (is (and (= (:bid @b1) {})
+                 (= (:ask @b1) {}))))))
 
-(let [o1 (order {:order-type :limit
-                 :side       :sell
-                 :price      109.8
-                 :amount     5
-                 :ticker     "aapl"})
-      o2 (order {:order-type :limit
-                 :side       :sell
-                 :price      109.9
-                 :amount     5
-                 :ticker     "aapl"})
-      o3  (order {:order-type :limit
-                  :side       :buy
-                  :price      110
-                  :amount     12
-                  :ticker     "aapl"})]
-  (execute-trade o3 (filter (price-acceptable 110 :buy :limit)
-                            (map vals [o1 o2]))))
+
