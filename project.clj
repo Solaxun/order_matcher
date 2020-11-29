3c45461c4584c8bfa7881c8ac7d80b0d58b2c887
solaxun <markcwoodworth@gmail.com>
2020-11-29 15:53:38 -0600

    update tests and gitignore

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure.java-time "0.3.2"]
-                 [org.clojure/data.priority-map "0.0.10"]]
+                 [org.clojure/data.priority-map "0.0.10"]
+                 [criterium "0.4.6"]
+                 [com.clojure-goes-fast/clj-async-profiler "0.4.1"]]
   :main ^:skip-aot order-matcher.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/order_matcher/orderbook_test.clj
+++ test/order_matcher/orderbook_test.clj
@@ -1,26 +1,110 @@
 (ns order-matcher.orderbook-test
   (:require [clojure.test :refer :all]
-            [order-matcher.orderbook :refer :all]
+            [order-matcher.orderbook :as orderbook]
             [order-matcher.protocols :as protocols]))
 
-(deftest limit-then-market
-  "Expect a resting limit order to be cleared by market, resulting in empty book"
-  (let [b1 (atom (new-fifo-book))
-        order1 (order {:order-type :limit
-                       :side       :bid
-                       :price      103.1
-                       :amount     5
-                       :ticker     "aapl"})
-        order2 (order {:order-type :market
-                       :side       :ask
-                       :price      104.1
-                       :amount     5
-                       :ticker     "aapl"})
-        _ (do (swap! b1 protocols/fill-order order1)
-              (swap! b1 protocols/fill-order order2))]
-
+(deftest limit-then-market-clear-book
+  "Expect a resting limit order that is matched with either an incoming market or limit order of the same quantity
+  to clear the book, resuting in no bids or asks (empty book)"
+  (let [b1 (orderbook/new-fifo-book "aapl")
+        order1 (orderbook/order {:order-type :limit
+                                 :side       :bid
+                                 :price      103.1
+                                 :amount     5
+                                 :ticker     "aapl"})
+        order2 (orderbook/order {:order-type :market
+                                 :side       :ask
+                                 :amount     5
+                                 :ticker     "aapl"})
+        order3 (orderbook/order {:order-type :limit
+                                 :side       :ask
+                                 :price      103.1
+                                 :amount     5
+                                 :ticker     "aapl"})
+        newbook (-> b1
+                    (protocols/fill-order order1)
+                    (protocols/fill-order order2))
+        newbook2 (-> b1
+                    (protocols/fill-order order1)
+                    (protocols/fill-order order3))]
     (testing
-        (is (and (= (:bid @b1) {})
-                 (= (:ask @b1) {}))))))
+      (is (and (= (:bid newbook) {})
+               (= (:ask newbook) {})))
+      (is (and (= (:bid newbook2) {})
+               (= (:ask newbook2) {})))
+      (is (= (vals (:price->quantity newbook))
+             [{} {}]))
+      (is (= (vals (:price->quantity newbook2))
+             [{} {}])))))
 
+(deftest limit-then-market-resting-book
+  "Expect a resting limit order that is matched with either an incoming market or limit order of the same quantity
+  to clear the book, resuting in no bids or asks (empty book)"
+  (let [b1 (orderbook/new-fifo-book "aapl")
+        order1 (orderbook/order {:order-type :limit
+                                 :side       :bid
+                                 :price      103.1
+                                 :amount     5
+                                 :ticker     "aapl"})
+        order2 (orderbook/order {:order-type :market
+                                 :side       :ask
+                                 :amount     12
+                                 :ticker     "aapl"})
+        order3 (orderbook/order {:order-type :limit
+                                 :side       :ask
+                                 :price      103.1
+                                 :amount     12
+                                 :ticker     "aapl"})
+        newbook (-> b1
+                    (protocols/fill-order order1)
+                    (protocols/fill-order order2))
+        newbook2 (-> b1
+                     (protocols/fill-order order1)
+                     (protocols/fill-order order3))]
+    (testing
+      (is (and (= (:bid newbook) {})
+               (= ((comp #(get % (:trade-id order2)) :ask) newbook)
+                  (-> order2 :trade (dissoc :price) (assoc :amount 7 :order-type :limit :price 103.1)))))
+      (is (and (= (:bid newbook2) {})
+               (= ((comp #(get % (:trade-id order3)) :ask) newbook2)
+                  (-> order3 :trade (assoc :amount 7)))))
+      (is (= (:price->quantity newbook)
+             {:bid {} :ask {103.1 7}}))
+      (is (= (:price->quantity newbook2)
+             {:bid {} :ask {103.1 7}})))))
 
+(defn test-limit-then-market []
+  (let [book (atom (orderbook/new-fifo-book "aapl"))
+        t1 (orderbook/order {:order-type :limit
+                             :side       :bid
+                             :price      114.1
+                             :amount     4
+                             :ticker     "aapl"})
+        t2 (orderbook/order {:order-type :limit
+                             :side       :bid
+                             :price      113.9
+                             :amount     1
+                             :ticker     "aapl"})
+        t3 (orderbook/order {:order-type :limit
+                             :side       :bid
+                             :price      113.8
+                             :amount     1
+                             :ticker     "aapl"})
+        t4 (orderbook/order {:order-type :limit
+                             :side       :ask
+                             :amount     10
+                             :price      10
+                             :ticker     "aapl"})]
+    (do
+      (swap! book protocols/fill-order t1)
+      (swap! book protocols/fill-order t2)
+      (swap! book protocols/fill-order t3)
+      (swap! book protocols/fill-order t4)
+      (swap! book protocols/cancel-order (:trade-id t4)))))
+
+(defn gen-order []
+  (orderbook/order {:ticker     "LTRPA"
+                    :side       (rand-nth [:bid :ask])
+                    :order-type (rand-nth [:limit :market])
+                    :amount     (rand-nth (range 10 50))
+                    :price      (* 10 (rand))}))
