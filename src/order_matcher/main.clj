35d9c1277b8f79144078b0032c36351650664617
solaxun <markcwoodworth@gmail.com>
2021-11-04 22:27:55 -0500

    wip perf tests in main

--- src/order_matcher/main.clj
+++ src/order_matcher/main.clj
@@ -1,12 +1,13 @@
 (ns order-matcher.main
   (:require [order-matcher.protocols :refer :all]
             [order-matcher.orderbook :as orderbook]
-            [order-matcher.display :as display]))
+            [order-matcher.display :as display]
+            [clojure.data.priority-map :refer [priority-map-keyfn priority-map]]))
 
 (def bookview (display/->FifoBookTableDisplay (sorted-map) (sorted-map)))
 (def aapl-book (orderbook/new-fifo-book "aapl"))
 
-(orderbook/fill-order aapl-book (orderbook/order {:order-type :limit
+(fill-order aapl-book (orderbook/order {:order-type :limit
                                                   :side       :bid
                                                   :price      114.1
                                                   :amount     4
@@ -34,11 +35,91 @@
                  :price      10
                  :ticker     "aapl"})]
   (do
-    (swap! book protocols/fill-order t1)
-    (swap! book protocols/fill-order t2)
-    (swap! book protocols/fill-order t3)
-    (swap! book protocols/fill-order t4)))
-(protocols/display-book
+    (swap! book fill-order t1)
+    (swap! book fill-order t2)
+    (swap! book fill-order t3)
+    (swap! book fill-order t4)))
+
+(defn rand-limit-order []
+  (orderbook/order {:order-type :limit
+                    :side       (rand-nth [ :bid :ask])
+                    :amount     (int (* 50 (rand)))
+                    :price      (Math/round (* 100 (rand)))
+                    :ticker     "aapl"}))
+
+(def sample-orders (for [i (range 1000000)] (rand-limit-order)))
+(def sample-orders-2 (for [i (range 200000)] (rand-limit-order)))
+
+;; 10 seconds (just adding) - should be fast as the other-side will always be nil
+;; so filtering won't start since the filtered collection is nul.  There should
+;; therefore never be a linear scan of the potential matching trades.  Yet this
+;; is still 10x slower than the simple conj example to priority map below - why?
+(time (let [b (atom (orderbook/new-fifo-book "aapl"))]
+        (doseq [o (take (* 1000 200) sample-orders)] (swap! b fill-order o))
+        #_(clojure.pprint/pprint @b)
+        #_(clojure.pprint/pprint (:ask @b))
+        ))
+
+;; 30 seconds for 20k vs 200k (all order types, both buy and sell)
+(time (let [b (atom (orderbook/new-fifo-book "aapl"))]
+        (doseq [o (take (* 1000 100) sample-orders-2)] (swap! b fill-order o))
+        #_(clojure.pprint/pprint @b)
+        #_(clojure.pprint/pprint (:ask @b))
+        #_(clojure.pprint/pprint (take 100 (:ask @b)))))
+
+;; 0.25 seconds
+(let [b {}]
+  (time (count (reduce conj
+                       b
+                       (map vec (partition 2 (range (* 1000 200))))))))
+;; 0.8 seconds
+(let [b (priority-map)]
+  (time (count (reduce conj
+                       b
+                       (map vec (partition 2 (range (* 1000 200))))))))
+
+(let [b (transient {})
+      c (map vec (partition 2 (range 1000000)))]
+  (time (count (persistent! (reduce conj! b c)))))
+
+
+(let [b (orderbook/new-fifo-book "aapl")
+      o1 (first sample-orders)
+      o2 (nth sample-orders 2)]
+  (-> (fill-order b o1)
+      (fill-order o2)
+      (clojure.pprint/pprint)))
+
+(doseq [i (take 3 sample-orders)]
+  (clojure.pprint/pprint i)
+  (println))
+
+(first sample-orders)
+(nth sample-orders 2)
+
+(let [b (orderbook/new-fifo-book "aapl")
+      o1 (orderbook/order {:order-type :limit
+                           :side       :bid
+                           :amount     100
+                           :price      25.20
+                           :ticker     "aapl"})
+      o2 (orderbook/order {:order-type :limit
+                           :side       :bid
+                           :amount     25
+                           :price      25.30
+                           :ticker     "aapl"})
+      o3 (orderbook/order {:order-type :limit
+                           :side       :ask
+                           :amount     75
+                           :price      25.20
+                           :ticker     "aapl"})]
+  (-> b
+      (fill-order o1)
+      (fill-order o2)
+      (fill-order o3)
+      (clojure.pprint/pprint)))
+
+(display-book
   #order_matcher.orderbook.FifoBookTableDisplay{:bid {}, :ask {10 4}})
 
 
